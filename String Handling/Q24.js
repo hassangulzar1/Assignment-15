345244c3b8456c0b0535f9646fe1b21b2a3ca69c
hassangulzar1 <hassangulzar121@gmail.com>
2023-04-27 16:41:00 +0500

    initial commit

--- String Handling/Q24.js
+++ String Handling/Q24.js
@@ -1,1 +1,6 @@
 // 24. Write a js program to remove all repeated characters from a given string.
+let string = "Hassan";
+let removeRepeated = (str) => {
+  let word = {};
+};
+console.log(removeRepeated(string));
